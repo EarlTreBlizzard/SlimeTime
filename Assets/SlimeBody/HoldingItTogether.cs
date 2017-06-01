30e6ed3b68d9c5e5ac5e76532f2e199c9e53f7f0
EarlTreBlizzard <treblizzard@gmail.com>
2017-06-01 19:32:49 -0400

    added head to the slime

---
+++ Assets/SlimeBody/HoldingItTogether.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldingItTogether : MonoBehaviour {
+
+	GameObject body;
+
+	// Use this for initialization
+	void Start () {
+		body = GameObject.FindGameObjectWithTag("SlimeHead");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	//	int len = body.Length;
+	//	float curr=0;
+
+	//	for(int i = 0;i<len;i++)
+	//	{
+	//		GameObject place;
+	//		place = body[i];
+	//		Vector2 holder = place.transform.position;
+	//		curr = curr + holder.x;
+	//	}
+
+	//	float avg;
+
+	//	avg = curr/len;
+
+		GetComponent<Rigidbody2D>().position = Vector2.MoveTowards(GetComponent<Rigidbody2D>().position, body.transform.position, .01f);
+		
+	}
+}
