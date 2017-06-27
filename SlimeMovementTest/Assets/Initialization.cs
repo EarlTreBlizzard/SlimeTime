206b968c36a9011c490d3b21753d3cfee35b9c68
Joshua Bevell <jbevl15@gmail.com>
2017-06-26 20:38:13 -0400

    Prototype of slime movement controls.

---
+++ SlimeMovementTest/Assets/Initialization.cs
@@ -0,0 +1,12 @@
+//Author: Joshua Bevell
+//
+//Initializes the screen to display well on an Android device for testing
+
+using UnityEngine;
+
+/*public class Initialization : MonoBehaviour
+{
+	public static Camera main;
+	public static GameObject slime;
+	public static SpriteRenderer slimesprite = gameObject.AddComponent<SpriteRenderer>() as SpriteRenderer;
+}*/
